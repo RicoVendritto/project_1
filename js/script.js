9c15c8381d3d830f5a81c9c5a2e048df9b1ea062
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-11-22 22:10:51 -0500

    First Logic Working - overview CL clubs homepage, eventListener to DIV, Next step -> details per club.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ let urlApiCL = "https://api.football-data.org/v2/competitions/CL/matches";
 let urlApiTeams = "https://api.football-data.org/v2/competitions/CL/teams";
 let urlApiClubs = "https://api.football-data.org/v2/teams/";
 
-const button = document.querySelector("#grabInput");
-button.addEventListener("click", processApi);
+// const button = document.querySelector("#grabInput");
+// button.addEventListener("click", processApi);
 const logoDiv = document.querySelector(".logo");
 const logoField = document.querySelector("#teamsCL");
+logoField.addEventListener("click", goGoGo);
 
 function processApi(evt) {
   evt.preventDefault();
@@ -30,7 +31,7 @@ async function overviewTeams(evt) {
   renderResults1(results);
 }
 
-async function getAllCompetitions(evt) {
+async function getAllCompetitions() {
   console.log("Api CL Call");
   let response = await axios.get(`https://cors-anywhere.herokuapp.com/${urlApiCL}`, {
     headers: {
@@ -41,47 +42,66 @@ async function getAllCompetitions(evt) {
   console.log(results);
 }
 
-async function getAllClubDetails(evt) {
+async function getAllClubDetails(id) {
   console.log("Api Club Call");
-  let teamID = 678;
+  let teamID = id;
   let response = await axios.get(`https://cors-anywhere.herokuapp.com/${urlApiClubs}${teamID}`, {
     headers: {
       'X-Auth-Token': api_key
     }
   });
   const results = response;
-  renderResults2(results);
+  console.log(results);
 }
 
 function renderResults1(results) {
   let data = results.data.teams;
+  // console.log(data);
   let i = 0;
   for (key in data) {
     let tempLogo = results.data.teams[i].crestUrl;
     let tempID = results.data.teams[i].id;
-    console.log(`${tempLogo} -- ${tempID}`);
+    let shortName = results.data.teams[i].shortName;
+    // console.log(`${tempLogo} -- ${tempID} -- ${shortName}`);
 
     if (!tempLogo) {
       tempLogo = "images/plain_logo.jpg";
     }
 
+    let teamDiv = document.createElement("div");
+    teamDiv.setAttribute("class", "teaminfo");
+    teamDiv.setAttribute("uniqueID", tempID);
+
     let imgThumb = document.createElement("img");
     imgThumb.setAttribute("src", tempLogo);
-    imgThumb.setAttribute("uniqueID", tempID);
     imgThumb.setAttribute("class", "clubLogo");
-    logoField.appendChild(imgThumb);
+    teamDiv.appendChild(imgThumb);
+
+    let teamTitle = document.createElement("p");
+    teamTitle.setAttribute("class", "teamname");
+    teamTitle.innerHTML = shortName;
+    teamDiv.appendChild(teamTitle);
+
+    logoField.appendChild(teamDiv);
 
     i++;
   }
 }
 
-function renderResults2(results) {
-  console.log(results);
-  let logo = results.data.crestUrl;
-  console.log(logo);
+// function renderResults2(results) {
+//   console.log(results);
+//   let logo = results.data.crestUrl;
+//   console.log(logo);
 
-  let imgLogo = document.createElement("img");
-  imgLogo.setAttribute("src", "images/plain_logo.jpg");
-  imgLogo.setAttribute("class", "teamLogo");
-  logoDiv.appendChild(imgLogo);
+//   let imgLogo = document.createElement("img");
+//   imgLogo.setAttribute("src", "images/plain_logo.jpg");
+//   imgLogo.setAttribute("class", "teamLogo");
+//   logoDiv.appendChild(imgLogo);
+// }
+
+function goGoGo(evt) {
+  console.log(evt);
+  console.log(evt.target.getAttribute("uniqueID"));
+  let id = evt.target.getAttribute("uniqueID");
+  getAllClubDetails(id)
 }
